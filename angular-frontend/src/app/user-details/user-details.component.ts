0092f01459b588e030cd710042bb909e2ff070e5
justbelieveinmyself <122427316+justbelieveinmyself@users.noreply.github.com>
2023-10-02 16:55:01 +0300

    modal forms bootstrap

--- angular-frontend/src/app/user-details/user-details.component.ts
+++ angular-frontend/src/app/user-details/user-details.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from '../service/token.service';
+import { LocalUser } from '../models/local-user';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['../user-profile/user-profile.component.css']
 })
-export class UserDetailsComponent {
-
+export class UserDetailsComponent implements OnInit {
+  user!: LocalUser;
+  newemail!: string;
+  constructor(
+    private tokenService: TokenService
+  ){}
+  ngOnInit(){
+    this.user = this.tokenService.getUser();
+  }
 }
