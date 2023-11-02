d569fccfa88baee433234bf4eaca2de766cf8a46
justbelieveinmyself <122427316+justbelieveinmyself@users.noreply.github.com>
2023-11-02 23:42:36 +0300

    Product cart final touches

--- angular-frontend/src/app/products/product-cart/product-cart.component.ts
+++ angular-frontend/src/app/products/product-cart/product-cart.component.ts
@@ -3,6 +3,7 @@ import {TokenService} from "../../service/token.service";
 import {ProductService} from "../../service/product.service";
 import {Product} from "../../models/product";
 import {ProductWithImage} from "../../models/product-with-image";
+import {LocalUser} from "../../models/local-user";
 
 @Component({
   selector: 'app-product-cart',
@@ -11,11 +12,14 @@ import {ProductWithImage} from "../../models/product-with-image";
 })
 export class ProductCartComponent implements OnInit{
     constructor(
-        private tokenService : TokenService,
-        private productService: ProductService
+        private tokenService: TokenService,
+        private productService: ProductService,
     ) {}
     isLogged = false;
-    products : ProductWithImage[];
+    user: LocalUser;
+    products: ProductWithImage[];
+    countOfProduct: number[];
+    totalCountOfProduct = 0;
     totalPrice = 0;
     ngOnInit(): void {
         this.tokenService.isLogged().subscribe(data => this.isLogged = data);
@@ -23,10 +27,26 @@ export class ProductCartComponent implements OnInit{
             next: products => {
                 this.products = products;
                 this.products.forEach(product => this.totalPrice+=product.product.price);
-                console.log(this.totalPrice)
+                this.countOfProduct = new Array(this.products.length).fill(1);
+                this.totalCountOfProduct = this.products.length;
             },
             error: err => console.log(err)
         })
+        this.user = this.tokenService.getUser();
     }
 
+    addNumberOfProduct(event : any){
+        const id = event.target.id;
+        this.countOfProduct[id]++;
+        this.totalPrice+=this.products[id].product.price;
+        this.totalCountOfProduct++;
+    }
+    removeNumberOfProduct(event : any){
+        const id = event.target.id;
+        if(this.countOfProduct[id] > 1) {
+            this.countOfProduct[id]--;
+            this.totalPrice -= this.products[id].product.price;
+            this.totalCountOfProduct--;
+        }
+    }
 }
